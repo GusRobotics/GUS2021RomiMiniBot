537da938f7a5d42266ee767d09fed932ddcf9a93
NPaul228 <Nwpaul2@icloud.com>
2021-02-27 12:53:40 -0500

    Slight drift compensation

--- MinibotCode/src/main/java/frc/robot/commands/OptimizedAuto.java
+++ MinibotCode/src/main/java/frc/robot/commands/OptimizedAuto.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class OptimizedAuto extends SequentialCommandGroup {
 
     public OptimizedAuto(Drivetrain driveBase) {
-        addCommands(new DriveDistance(0.5, 13.5, driveBase), new TurnDegrees(0.5, 270, driveBase),
+        addCommands(new DriveDistance(0.5, 13.5, driveBase), new TurnDegrees(0.5, -90, driveBase),
                 new DriveDistance(0.5, 13.5, driveBase), new TurnDegrees(0.5, 270, driveBase),
                 new DriveDistance(0.5, 10.5, driveBase), new TurnDegrees(0.5, 270, driveBase),
                 new DriveDistance(0.5, 9, driveBase), new TurnDegrees(0.5, 90, driveBase),
